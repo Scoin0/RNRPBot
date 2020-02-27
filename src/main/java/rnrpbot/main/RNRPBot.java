617dab413c1831f859ab458dccf7867d99af5dc9
Scoin0 <tehscoinhub@gmail.com>
2020-02-26 16:02:30 -1000

    Beginning Stage

---
+++ src/main/java/rnrpbot/main/RNRPBot.java
@@ -0,0 +1,13 @@
+package rnrpbot.main;
+
+import rnrpbot.utils.Constants;
+
+public class RNRPBot {
+
+    public static final long startTime = System.currentTimeMillis();
+
+    public static void main(String[] args) {
+        System.out.println(Constants.getLogo());
+    }
+
+}
---
+++ src/main/java/rnrpbot/utils/Constants.java
@@ -0,0 +1,21 @@
+package rnrpbot.utils;
+
+public class Constants {
+
+    //Bot Information
+    public static final String BOT_NAME = "RNRPBot";
+
+    public static String getLogo() {
+        return "______ _   _ __________________       _   \r\n" +
+                "| ___ \\ \\ | || ___ \\ ___ \\ ___ \\     | |  \r\n" +
+                "| |_/ /  \\| || |_/ / |_/ / |_/ / ___ | |_ \r\n" +
+                "|    /| . ` ||    /|  __/| ___ \\/ _ \\| __|\r\n" +
+                "| |\\ \\| |\\  || |\\ \\| |   | |_/ / (_) | |_ \r\n" +
+                "\\_| \\_\\_| \\_/\\_| \\_\\_|   \\____/ \\___/ \\__|"
+                + "\nCurrent Version: " + "ADD LATER"
+                + "\nLatest Version: " + "ADD LATER"
+                + "\nJDA Version: " + "ADD LATER"
+                + "\nJVM: " + System.getProperty("java.version")
+                + "\n";
+    }
+}
